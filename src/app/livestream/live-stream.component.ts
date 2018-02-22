bb46fbb7942fab3161463ba9ddc29d204457fd85
Filip Bech-Larsen <filipbech@filipbech.dk>
2018-02-22 21:00:38 +0100

    youtube embed

--- src/app/livestream/live-stream.component.ts
+++ src/app/livestream/live-stream.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-live-stream',
   template: `
-    Show the youtube embed?
+    <h1>Livestream</h1>
+    <p>this is currently from ÅVM, but should show from SommerOase</p>
+    <iframe
+      frameborder="0"
+      allow="autoplay; encrypted-media"
+      src="//www.youtube.com/embed/live_stream?channel=UCgkGpoUYb7M543ZzLjAKaIQ"
+      allowfullscreen></iframe>
   `
 })
 export class LiveStreamComponent {
