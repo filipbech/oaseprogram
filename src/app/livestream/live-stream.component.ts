5fd4cd455b916f10f8840e47af61676b5d073afd
Filip Bech-Larsen <filipbech@filipbech.dk>
2018-03-23 21:52:07 +0100

    livestream

--- src/app/livestream/live-stream.component.ts
+++ src/app/livestream/live-stream.component.ts
@@ -5,11 +5,13 @@ import { Component } from '@angular/core';
   template: `
     <h1>Livestream</h1>
     <p>this is currently from ÅVM, but should show from SommerOase</p>
-    <iframe
-      frameborder="0"
-      allow="autoplay; encrypted-media"
-      src="//www.youtube.com/embed/live_stream?channel=UCgkGpoUYb7M543ZzLjAKaIQ"
-      allowfullscreen></iframe>
+    <div class="video-container">
+      <iframe
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        src="//www.youtube.com/embed/live_stream?channel=UCgkGpoUYb7M543ZzLjAKaIQ"
+        allowfullscreen></iframe>
+    </div>
   `
 })
 export class LiveStreamComponent {
