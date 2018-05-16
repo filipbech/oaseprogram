1054964d0120a8e6dd6d93c310675020cef9b424
Filip Bech-Larsen <filipbech@filipbech.dk>
2018-05-16 08:16:58 +0200

    simple serviceworker

--- src/sw.js
+++ src/sw.js
@@ -35,39 +35,47 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
 
-  event.respondWith(new Promise(resolve => {
+  if (event.request.url.indexOf('google-analytics') > -1) {
+    return;
+  }
 
-    if(event.request.url.indexOf('google-analytics') > -1) {
-      resolve(fetch(event.request));
-    }
+  event.respondWith(caches.match(event.request).then(cacheResponse => cacheResponse || fetch(event.request)));
 
-    // FOR navigation-fetches, go NETWORK-FIRST with a max-reponsetime of 500ms
-    if (event.request.mode == 'navigate') {
-      const MAX_TIME = new Promise(res => {
-        setTimeout(_=>{
-          console.log('timeout');
-          res();
-        }, 500);
-      });
-      return resolve(Promise.race([fetch(event.request)
-        .then(fetchResponse => {
-          return caches.open(APP_CACHE_NAME)
-            .then(cache => cache.put(event.request, fetchResponse.clone()))
-            .then(_ => fetchResponse);
-        }), MAX_TIME.then(_ => caches.match('/'))
-      ]));
-    }
 
-    // ALL OTHER REQUESTS ARE SERVED CACHE-FIRST
-    return resolve(caches.match(event.request)
-      .then(cacheResponse => cacheResponse || fetch(event.request)
-        .then(fetchResponse => {
-          return caches.open(STATIC_CACHE_NAME)
-            .then(cache => cache.put(event.request, fetchResponse.clone()))
-            .then(_ => fetchResponse);
-        })
-      )
-    );
-  }));
+
+//   event.respondWith(new Promise(resolve => {
+
+//     if(event.request.url.indexOf('google-analytics') > -1) {
+//       resolve(fetch(event.request));
+//     }
+
+//     // FOR navigation-fetches, go NETWORK-FIRST with a max-reponsetime of 500ms
+//     if (event.request.mode == 'navigate') {
+//       const MAX_TIME = new Promise(res => {
+//         setTimeout(_=>{
+//           console.log('timeout');
+//           res();
+//         }, 500);
+//       });
+//       return resolve(Promise.race([fetch(event.request)
+//         .then(fetchResponse => {
+//           return caches.open(APP_CACHE_NAME)
+//             .then(cache => cache.put(event.request, fetchResponse.clone()))
+//             .then(_ => fetchResponse);
+//         }), MAX_TIME.then(_ => caches.match('/'))
+//       ]));
+//     }
+
+//     // ALL OTHER REQUESTS ARE SERVED CACHE-FIRST
+//     return resolve(caches.match(event.request)
+//       .then(cacheResponse => cacheResponse || fetch(event.request)
+//         .then(fetchResponse => {
+//           return caches.open(STATIC_CACHE_NAME)
+//             .then(cache => cache.put(event.request, fetchResponse.clone()))
+//             .then(_ => fetchResponse);
+//         })
+//       )
+//     );
+//   }));
 
 });
