ab2c545bfa3727372a2081102fca42817d19bbf9
Filip Bech-Larsen <filipbech@Filips-MacBook-Pro-M2.local>
2023-07-15 00:32:13 +0200

    2023

--- src/app/livestream/live-stream.component.ts
+++ src/app/livestream/live-stream.component.ts
@@ -4,14 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-live-stream',
   template: `
     <h1>Livestream</h1>
-    <p>Her kan du se live-video fra årets sommeroase. Du kan finde optagelser fra i år og tidligere år på <a href="https://www.youtube.com/c/danskoase" rel="noopener" target="_blank">vores youtube kanal</a>.</p>
-    <div class="video-container">
-      <iframe
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        src="//www.youtube.com/embed/live_stream?channel=UC8-K5ImW5BxS07OgpmSlyPQ"
-        allowfullscreen></iframe>
-    </div>
+    <p>Her kan du se live-video fra årets sommeroase. Du kan finde optagelser fra i år og tidligere år på <a href="https://www.youtube.com/channel/UC8-K5ImW5BxS07OgpmSlyPQ/live" rel="noopener" target="_blank">vores youtube kanal</a>.</p>
+    <a href="https://www.youtube.com/channel/UC8-K5ImW5BxS07OgpmSlyPQ/live" rel="noopener" target="_blank" class="stream-image">
+      <img src="/assets/stream.jpg" />
+    </a>
   `
 })
 export class LiveStreamComponent {
